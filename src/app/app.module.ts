5d33a9bfa639fc493e756844ebcbef37eb12ae83
Fatomi02 <fatomiabdulrahmon@gmail.com>
2023-10-22 16:25:42 -0400

    Navbar Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
 
 import { AppComponent } from './app.component';
+import { ComponentModule } from './Movie-Code/movie-code.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, ComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
